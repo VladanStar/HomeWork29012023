e069a97d9031830d9c0693bdc098433e8a79936b
VladanStar <vladancupric@gmail.com>
2023-01-29 20:21:38 +0100

    Add

--- task_js/index.js
+++ task_js/index.js
@@ -1,11 +1,26 @@
-let arr =[1,2,3,4,5,6,7,8,9,]
-      function maxNumber(arr) {
-        let max = arr[0];
-        for (let i = 1; i < arr.length; i++) {
-          if (arr[i] > max) {
-            max = arr[i];
-          }
-        }
-        return max;
-      }
-      console.log(maxNumber(arr))
+//Napišite funkciju koja će primiti niz brojeva i vratiti najveći broj u nizu.
+let arr = [1, 2, 78, 3, 4, 5, 6, 7, 8, 9];
+function maxNumber(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+console.log(maxNumber(arr));
+
+//Napišite funkciju koja će primiti niz stringova i vratiti string sa najvećim brojem karaktera.
+let str1 = ["Vladan","Bojan", "Predrag","Ognjen"];
+
+function longestString(arr) {
+  let longest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].length > longest.length) {
+      longest = arr[i];
+    }
+  }
+  return longest;
+}
+console.log(longestString(str1));
